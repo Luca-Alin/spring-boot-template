769a0e5396524d193e7e1882835fc963df6595d0
alin-luca <alinluca20021@gmail.com>
2024-08-15 22:47:57 +0300

    Replaced MySQL and JPA configuration and annotations with MongoDB

--- src/main/java/springboottemplate/data/user/User.java
+++ src/main/java/springboottemplate/data/user/User.java
@@ -1,10 +1,12 @@
 package springboottemplate.data.user;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,18 +18,17 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name="users")
+@Document
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private String password;
-    @Enumerated(EnumType.STRING)
     private Role role;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
--- src/main/java/springboottemplate/data/user/UserRepository.java
+++ src/main/java/springboottemplate/data/user/UserRepository.java
@@ -1,12 +1,10 @@
 package springboottemplate.data.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends MongoRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
